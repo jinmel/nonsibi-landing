d4cf28426891d0e3312ed274373299f18da5be20
Hank Lee <hank.lee@kakaocorp.com>
2017-08-22 18:15:05 +0900

    Update after Symphonia event. Shortened [Experience] section text.  Changed video to Symphonia.  Added Facebook group Link

--- js/register.js
+++ js/register.js
@@ -7,15 +7,19 @@
 
 // https://stackoverflow.com/questions/13437091/is-it-possible-to-not-load-an-iframe-in-a-hidden-div-until-the-div-is-displayed
 
-function toggle_registration() {
-    // TODO: Get iframe by element id
-    var reg_form_iframe = document.getElementById('register-form')
+function toggle_onclick(elemIdList) {
+    // Get iframe by element id
+    // var toggle_elem = document.getElementById('register-form')
+    for(var i = 0; i < elemIdList.length; i++)
+    {
+        var toggle_elem = document.getElementById(elemIdList[i])
 
-    // TODO: Set 'hidden' to opposite of current
-    if ( reg_form_iframe.hidden === true ) {
-        reg_form_iframe.hidden = false
-    } else {
-        reg_form_iframe.hidden = true
+        // Set 'hidden' to opposite of current
+        if ( toggle_elem.hidden === true ) {
+            toggle_elem.hidden = false
+        } else {
+            toggle_elem.hidden = true
+        }
     }
 
 }
